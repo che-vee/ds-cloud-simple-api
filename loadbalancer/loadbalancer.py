4033369731357b26910b34d2c73f4ac39c70536e
Valentine <VEE.LANGAT@GMAIL.COM>
2023-06-21 00:29:07 +0200

    update loadbalancer

---
+++ loadbalancer/loadbalancer.py
@@ -0,0 +1,69 @@
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import RedirectResponse
+from itertools import cycle
+
+import logging
+import requests
+import time
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.StreamHandler()
+    ]
+)
+
+APP_URLS = [
+    "http://localhost:8081",
+    "http://localhost:8082",
+    "http://localhost:8083",
+]
+
+healthy_apps = []
+app_urls_cycle = cycle(healthy_apps)
+
+def get_healthy_apps():
+    global healthy_apps, app_urls_cycle
+
+    for app_url in APP_URLS:
+        try:
+            response = requests.get(f"{app_url}/health")
+            if response.status_code == 200:
+                logging.info(f"Health check passed for app instance: {app_url}")
+                if app_url not in healthy_apps:
+                    healthy_apps.append(app_url)
+            else:
+                if app_url in healthy_apps:
+                    healthy_apps.remove(app_url) 
+        except requests.RequestException as e:
+            logging.warning(f"Health check failed for app instance: {app_url}. Error: {str(e)}")
+
+    app_urls_cycle = cycle(healthy_apps)
+
+        
+def redirect_request(request: Request):
+    if not healthy_apps:
+        logging.error("No healthy apps available.")
+        raise HTTPException(status_code=503, detail="No healthy apps available.")
+    next_app_url = next(app_urls_cycle)
+    try:
+        request_path = request.url.path
+        redirect_url = f"{next_app_url}{request_path}"
+
+        logging.info(f"Redirecting request to: {redirect_url}")
+        return RedirectResponse(url=redirect_url, status_code=307)
+    except Exception as e:
+        logging.error(f"Error during request redirection: {str(e)}")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+    
+app = FastAPI()
+
+@app.on_event("startup")
+async def startup_event():
+    app.add_event_handler("startup", get_healthy_apps)
+    
+@app.middleware("http")
+async def load_balancer_middleware(request: Request, call_next):
+    return redirect_request(request)
+
--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-from fastapi import FastAPI, Request, status
+from fastapi import FastAPI
 from app.coffee import router as coffee_router
-from load_balancer import redirect_request
-from typing import Callable
 
 
 app = FastAPI()
-@app.middleware("http")
-async def load_balance_middleware(request: Request, call_next: Callable):
-    response = await call_next(request)
-    if response.status_code == 200:
-        return response
-    else:
-        return redirect_request(request)
        
 app.include_router(coffee_router)
