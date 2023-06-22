c1775884628c91e142d88749113e1c4a3d1aae58
Valentine <VEE.LANGAT@GMAIL.COM>
2023-06-23 01:38:58 +0200

    fix create user endpoint

--- app/coffee.py
+++ app/coffee.py
@@ -58,8 +58,7 @@ def set_favourite_coffee(body: Dict[str, str], user: str = Depends(authenticate)
     else:
         new_user = User(username=user, favourite_coffee=favourite_coffee)
         session.add(new_user)
-
-    session.commit()
+        session.commit()
 
     top_three = get_favourite_coffees_count(session)
     return {"data": {"favourite_coffee": favourite_coffee, "top_three": top_three}}
@@ -71,14 +70,13 @@ def create_user(body: Dict[str, str], user: str = Depends(authenticate), session
     if not username or not email:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User info not provided.")
 
-    existing_user = get_user_info(user, session)
+    existing_user = get_user_info(username, session)
     if existing_user:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Username already exists.")
-    else:
-        new_user = User(username=username, email=email)
-        session.add(new_user)
-
+    
+    new_user = User(username=username, email=email)
+    session.add(new_user)
     session.commit()
     
-    message = {"user_id": user.id, "email": user.email}
+    message = {"user_id": new_user.id, "email": new_user.email}
     return {"message": "User created and sent for processing"}
